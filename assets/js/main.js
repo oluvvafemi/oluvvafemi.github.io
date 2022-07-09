436c41e693894985b820e23b3d4e4f6ec2e39aa8
Chris Adebiyi <oluvvafemi@gmail.com>
2022-07-09 10:36:11 +0200

    Set dark-mode at os-level with ability to change

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,18 @@
-function modeToggle() {
-  pageBody.classList.toggle('dark-mode');
-  counter++
-  modeSwitch.src = modeIcons[counter%2]
+let modeToggle = () =>  {
+  if (prefersDarkScheme.matches) {
+    document.body.classList.toggle('light-mode');
+  } else {
+    document.body.classList.toggle('dark-mode');
+  }
+  // document.body.classList.toggle('dark-mode');
+  // counter++
+  // modeSwitch.src = modeIcons[counter%2]
 }
 
-let pageBody = document.body;
-let modeSwitch = document.querySelector('#mode-switch')
+// let pageBody = document.body;
+const modeSwitch = document.querySelector('#mode-switch')
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 let modeIcons = ['assets/images/mode/moon.png','assets/images/mode/sun-64.png']
-let counter = 0
+// let counter = 0
 
 modeSwitch.addEventListener('click', modeToggle )
